6c283592d422cfaea1b3ab156de23c29eaca9d08
Jefwillems <jefwillems100@gmail.com>
2018-03-06 10:47:33 +0100

    add app secret and id from process.env . updated test script

--- src/stats.js
+++ src/stats.js
@@ -1,5 +1,6 @@
 const request = require("request");
-
+const gh_id = process.envGH_CLIENT_ID;
+const gh_secret = process.env.GH_CLIENT_SECRET;
 class DataService {
   constructor() {
     this.reposUrl = "https://api.github.com/orgs/lab9k/repos";
@@ -7,7 +8,7 @@ class DataService {
 
   getJSON(address, cb) {
     request(
-      address,
+      address + `?client_id=${gh_id}&client_secret=${gh_secret}`,
       { json: true, headers: { "User-Agent": "Lab9k" } },
       (err, res, body) => {
         if (err) {
@@ -19,43 +20,34 @@ class DataService {
   }
 
   checkRepoActivity(repo) {
-      
     let date = repo["pushed_at"];
 
     //console.log(date);
 
     date = Date.parse(date);
-    
-    let currentDate = (new Date()).getTime() - (7 * 24 * 60 * 60 * 1000);
-    //return false;
-    return (currentDate < date);
-  }
-  
-  fetchData() {
-      this.getJSON(this.reposUrl, data => {
-      
-          //Overloop de array, neem de actieve repos (laatste activiteit max 1 week geleden)
-          data = data.filter(this.checkRepoActivity);
-          //TODO: Overloop elke actieve repo om de contributorsinfo op te lijsten
-          data.forEach(e => {
-              let contsPerRepoAddress = "https://api.github.com/repos/lab9k/" + e.name + "/stats/contributors"
 
-              this.getJSON(contsPerRepoAddress, data => {
-                 /*data.forEach(e => {
+    let currentDate = new Date().getTime() - 7 * 24 * 60 * 60 * 1000;
+    //return false;
+    return currentDate < date;
+  }
+
+  fetchData() {
+    this.getJSON(this.reposUrl, data => {
+      //Overloop de array, neem de actieve repos (laatste activiteit max 1 week geleden)
+      data = data.filter(this.checkRepoActivity);
+      //TODO: Overloop elke actieve repo om de contributorsinfo op te lijsten
+      data.forEach(e => {
+        let contsPerRepoAddress = `https://api.github.com/repos/lab9k/${
+          e.name
+        }/stats/contributors`;
+
+        this.getJSON(contsPerRepoAddress, data => {
+          /*data.forEach(e => {
                    console.log(e);
                  })*/
-                 console.log(typeof JSON.parse(data));
-              })
-
-
-            
-
-        
-        
-            
-          });
-
-      
+          console.log(JSON.parse(JSON.stringify(data)));
+        });
+      });
     });
   }
 }
