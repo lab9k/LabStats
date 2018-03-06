385d700f54cc0f8c99fe50a4b59a6ad00098851a
RubenBruggeman <ruben.bruggeman.v8697@student.hogent.be>
2018-03-06 13:33:38 +0100

    aanpassingen

--- src/stats.js
+++ src/stats.js
@@ -53,14 +53,29 @@ class DataService {
         //TODO: overloop voor elke contributor zijn activiteit van de laatste 5 weken
         this.getJSON(contsPerRepoAddress, data => {
           data.forEach(d => {
+            let weken = d.weeks;
             let author = d.author.login;
 
-            let weeksWhereCommitsHappened = d.weeks.filter(data => {
+            weken = weken.filter(data => {
               return data["c"] >= 1;
             });
-            let weken = weeksWhereCommitsHappened.filter(data => {
-              return this.compareDates(data["w"] * 1000, 35, false);
+            console.log(
+              `${author} commits: OUDE WEKEN voor ${e.name}:` + weken.length
+            );
+
+            weken = weken.filter(data => {
+              let date = data["w"];
+              let currentDate = new Date().getTime() - 35 * 24 * 60 * 60 * 1000;
+              let result = this.compareDates(data["w"], 35, false);
+              console.log(
+                `Date: ${date} , currentDate: ${currentDate} , result: ${result}`
+              );
+              return this.compareDates(data["w"], 35, false);
             });
+
+            console.log(
+              `${author} commits: NIEUWE WEKEN voor ${e.name}:` + weken.length
+            );
           });
         });
       });
@@ -75,6 +90,11 @@ class DataService {
 
 
         this.getJSON(commitActivity, data => {
+
+          console.log("========================================================================================")
+          console.log(e.name)
+          console.log("========================================================================================")
+
           data.forEach(ca => {
             console.log(ca);
           })
