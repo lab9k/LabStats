6b1edff9701db91be90fcc2879babbf1f2a89bcc
Ruben Alliet <allietruben@gmail.com>
2018-03-06 15:31:11 +0100

    complete markdown backbone for project post

--- src/generator.js
+++ src/generator.js
@@ -1,6 +1,6 @@
 class BlogPostGenerator {
   constructor() {}
-  markdown = 
+  markdownlayout = 
   //Lab9k logo
   "<img src='https://lab9k.github.io/images/logo.svg' width='170'>\n" +
   "# Lab9k Github report (Insert date range here)\n" +
@@ -14,6 +14,7 @@ class BlogPostGenerator {
   //Contributors
   "### 1.2.Contributors\n" + 
   "All the people that contributed to this project.\n" +
+  //Iterate over all  users
   "* [User 1](https://github.com/rubenalliet)\n\n" +
   //Commits
   "### 1.3.Commits\n" +
@@ -26,15 +27,13 @@ class BlogPostGenerator {
   //Kan ban board todo board
   "### 1.4.Kanban Board | Progress\n\n" +
   "This board gives an overview of the current state of the project.\n\n";
+  //Iterate over all tasks of kanban board 
+  "| Todo               |In progress         | Testing            | Done                | \n";
+  "|--------------------|--------------------|--------------------|---------------------| \n\n";
+  "|Item 1              |Item 2              |Item 3              |Item 4               | \n\n";
 
-
-
-
-
-
-  
-
-
+  //URL to kanban board
+  "To get a complete overview click [here](https://github.com/lab9k/Skos/projects/1)\n";
 
   
   ;
