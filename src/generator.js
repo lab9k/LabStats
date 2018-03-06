a09e3f422f2e44008e1cc0656245920858fe0b1d
Ruben Alliet <allietruben@gmail.com>
2018-03-06 17:26:58 +0100

    fs.open fs.writefile

--- src/generator.js
+++ src/generator.js
@@ -1,12 +1,29 @@
+var fs = require("fs");
+
 class BlogPostGenerator {
   createPost() {
-    //pseudocode
-    //fs.open(markdownfile)
-    //recognize {{  }}
+    var template = "";
+    //read markdown template as a string. 
+    fs.readFile("../templates/template.md", function (err, data) {
+      if(err) {
+        return console.log(err);
+      }
+      template = data.toString();
+    });
+    
+    //fetch data from stats.js
+    //Recognize parameters {{ }} & replace them with values from stats.js
+    //template.replace(regexp|substr, newSubstr|function);
+
+    //Store String as a markdown file.
+    fs.writeFile("../templates/testreport.md", template, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+    }); 
     
   }
-
-
 
 }
 
